18fb7187805aebb1af8ecc31130264785d1d99e6
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-07-31 02:57:18 +0500

    frist commit from lec

---
+++ Ch6-P1-BasicInheritance/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch6_P1_BasicInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" LeC 6 - Inheritance ");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
