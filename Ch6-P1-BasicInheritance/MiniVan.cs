1f09f2b20061d2b66df24e9d348d895f179e75a0
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-07-31 03:15:39 +0500

    3rd commit from LEC

---
+++ Ch6-P1-BasicInheritance/MiniVan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch6_P1_BasicInheritance
+{
+    // MiniVan "is-a" Car
+    class MiniVan : Car
+    {
+        public void TestMethod()
+        {
+            // OK! Can access public members
+            // of a parent within a derived type.
+            Speed = 10;
+            // Error! Cannot access private
+            // members of parent within a derived type.
+            //currSpeed = 10;
+        }
+    }
+}
--- Ch6-P1-BasicInheritance/Program.cs
+++ Ch6-P1-BasicInheritance/Program.cs
@@ -10,8 +10,26 @@ namespace Ch6_P1_BasicInheritance
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" LeC 6 - Inheritance ");
+            Console.WriteLine("***** Basic Inheritance *****\n");
 
+            //// Make a Car object and set max speed.
+            //Car myCar = new Car(120);
+            //// Set the current speed, and print it.
+            //myCar.Speed = 90;
+            //Console.WriteLine("My car is going {0} Km/h", myCar.Speed);
+
+
+            #region Specifying the Parent Class of an Existing Class
+
+            // Now make a MiniVan object.
+            MiniVan myVan = new MiniVan();
+            myVan.Speed = 20;
+            Console.WriteLine("My van is going {0} Km/h", myVan.Speed);
+
+            //// Error! Can't access private members!
+            //myVan.currSpeed = 55;
+
+            #endregion
 
 
             Console.ReadLine();
